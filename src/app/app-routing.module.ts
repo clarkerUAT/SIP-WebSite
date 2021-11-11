063519d1ea8c8a05fcf20bcbfd52702677b7b214
Ray <64998877+clarkerUAT@users.noreply.github.com>
2021-11-10 22:28:40 -0600

    SIP-WEB

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'about me', component:AboutMeComponent},
+  {path:'home page', component:HomePageComponent,}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
